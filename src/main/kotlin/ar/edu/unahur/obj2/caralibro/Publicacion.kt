885f823268519a4c7b20ec9d4591ba8d90899f18
patriciorearte <patriciorearte97@gmail.com>
2021-04-17 21:19:59 -0300

    Falta como saber quienes le dieron mg ala publicacion

--- src/main/kotlin/ar/edu/unahur/obj2/caralibro/Publicacion.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/caralibro/Publicacion.kt
@@ -2,19 +2,24 @@ package ar.edu.unahur.obj2.caralibro
 
 import kotlin.math.ceil
 
-//Si lees esto es porque funciona el pull
 
 abstract class Publicacion
 {
+
   abstract fun espacioQueOcupa(): Int
+  abstract fun cantidadDeMegusta():Int
+
+
 }
 
 
 class Foto(val alto: Int, val ancho: Int) : Publicacion()
 {
-    var factorDeCompresion = 0.7
+  var factorDeCompresion = 0.7
+  var meGustaObetenidosActuales=0
   override fun espacioQueOcupa() = ceil(alto * ancho * factorDeCompresion).toInt()
-
+  override fun cantidadDeMegusta(): Int =meGustaObetenidosActuales
+  fun darMegusta(){meGustaObetenidosActuales +=1}
   fun cambiarFactorDeComprension(nuevoFactorDeCompresion:Double)
   {
       factorDeCompresion = nuevoFactorDeCompresion
@@ -23,13 +28,18 @@ class Foto(val alto: Int, val ancho: Int) : Publicacion()
 
 class Texto(val contenido: String) : Publicacion()
 {
+  var meGustaObetenidosActuales=0
+  override fun cantidadDeMegusta(): Int =meGustaObetenidosActuales
   override fun espacioQueOcupa() = contenido.length
+  fun darMegusta(){meGustaObetenidosActuales +=1}
 }
 //Es este archivo
 class Video(val duracion:Int, var calidad:Calidad): Publicacion()
 {
+    var meGustaObetenidosActuales=0
+    override fun cantidadDeMegusta(): Int =meGustaObetenidosActuales
     override fun espacioQueOcupa(): Int = calidad.espacioQueOcupa(duracion)
-
+    fun darMegusta(){meGustaObetenidosActuales +=1}
 }
 abstract class Calidad {
     abstract fun espacioQueOcupa(duracion:Int):Int
--- src/test/kotlin/ar/edu/unahur/obj2/caralibro/UsuarioTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/caralibro/UsuarioTest.kt
@@ -4,32 +4,39 @@ import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 
 class UsuarioTest : DescribeSpec({
-  describe("Caralibro") {
+  describe("Caralibro")
+  {
     val saludoCumpleanios = Texto("Felicidades Pepito, que los cumplas muy feliz")
     val fotoEnCuzco = Foto(768, 1024)
+    val videoDeLaUniversidad= Video(3,HD720p)
 
 
     describe("Una publicación") {
-      describe("de tipo foto") {
-        it("ocupa ancho * alto * compresion bytes") {
+      describe("de tipo foto")
+      {
+        it("ocupa ancho * alto * compresion bytes")
+        {
           fotoEnCuzco.espacioQueOcupa().shouldBe(550503)
+          fotoEnCuzco.cantidadDeMegusta().shouldBe(2)
         }
       }
 
 
       describe("de tipo texto") {
-        it("ocupa tantos bytes como su longitud") {
+        it("ocupa tantos bytes como su longitud")
+        {
           saludoCumpleanios.espacioQueOcupa().shouldBe(45)
+          saludoCumpleanios.cantidadDeMegusta().shouldBe(0)
         }
       }
 
       describe("un video") {
-        it("ocupa tantos bytes como su longitud") {
-          val videoDeLaUniversidad= Video(3,HD720p)
+        it("ocupa tantos bytes como su longitud")
+        {
           videoDeLaUniversidad.espacioQueOcupa().shouldBe(9)
           videoDeLaUniversidad.calidad = HD1080p
           videoDeLaUniversidad.espacioQueOcupa().shouldBe(18)
-
+          videoDeLaUniversidad.cantidadDeMegusta().shouldBe(0)
         }
       }
     }
@@ -37,12 +44,16 @@ class UsuarioTest : DescribeSpec({
     describe("Un usuario") {
       it("puede calcular el espacio que ocupan sus publicaciones") {
         val juana = Usuario()
+        val brenda = Usuario()
         juana.agregarPublicacion(fotoEnCuzco)
         juana.agregarPublicacion(saludoCumpleanios)
         juana.espacioDePublicaciones().shouldBe(550548)
+        juana.darMeGusta(FotoEnCuzco)
+        brenda.darMeGusta(FotoEnCuzco)
       }
 
     }
   }
+
 })
 
