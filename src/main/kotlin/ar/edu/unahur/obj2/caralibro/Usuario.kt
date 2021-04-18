4f366f57ba921f2a7e93c8d3a930203f6b77f41e
Brenda <brenda.denhoff@estudiantes.unahur.edu.ar>
2021-04-18 13:33:17 -0300

    Mejoras hechas

--- src/main/kotlin/ar/edu/unahur/obj2/caralibro/Usuario.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/caralibro/Usuario.kt
@@ -5,16 +5,32 @@ class Usuario {
 
   val amigos = mutableListOf<Usuario>()
 
+  val publicacionesQueMeGustan = mutableListOf<Publicacion>()
+
   fun agregarPublicacion(publicacion: Publicacion) = publicaciones.add(publicacion)
 
-  fun darMeGusta(publicacion: Publicacion) = publicacion.recibirMeGusta()
+  fun darMeGusta(publicacion: Publicacion) {
+    if(publicacionesQueMeGustan.contains(publicacion)) {
+      error("Ya le diste me gusta a esta publicacion")
+    }
+    else {
+      publicacion.recibirMeGusta()
+    }
+  }
 
   fun espacioDePublicaciones() = publicaciones.sumBy { it.espacioQueOcupa() }
 
-  fun agregarAmigo(amigo: Usuario) = amigos.add(amigo)
+  fun agregarAmigo(amigo: Usuario) {
+    if (amigos.contains(amigo)) {
+      error("Ya es tu amigo.")
+    } else {
+      amigos.add(amigo)
+    }
+  }
 
   fun esMasAmistoso(usuario: Usuario) = this.cantidadDeAmigos() > usuario.cantidadDeAmigos()
 
   fun cantidadDeAmigos() = amigos.size
+
 
 }
